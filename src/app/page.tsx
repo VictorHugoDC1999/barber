956ef04da3795e210420c30a16a646768163a25a
VictorHugoDC1999 <victorhugodelciampo@gmail.com>
2024-08-05 17:38:01 -0300

    :wrench: chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-const Home = () => {
-  return <div>Home</div>;
-};
+import { Button } from "./_components/ui/button"
 
-export default Home;
+const Home = () => {
+  return (
+    <div>
+      Home
+      <Button>teste</Button>
+    </div>
+  )
+}
+
+export default Home
