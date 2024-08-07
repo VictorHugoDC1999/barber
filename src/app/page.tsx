f33b31d8c47473e5dc4af7bf05b2dc58de3a0dd3
VictorHugoDC1999 <victorhugodelciampo@gmail.com>
2024-08-07 15:06:37 -0300

    feat: :sparkles: add booking item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import Header from './_components/Header'
 import { Button } from './_components/ui/button'
 import { Input } from './_components/ui/input'
 import Image from 'next/image'
+import { Card, CardContent } from './_components/ui/card'
+import { Badge } from './_components/ui/badge'
+import { Avatar, AvatarImage } from './_components/ui/avatar'
 
 const Home = () => {
   return (
@@ -10,11 +13,12 @@ const Home = () => {
       {/* header */}
       <Header />
 
-      {/*  */}
       <div className="p-5">
+        {/* Texto */}
         <h2 className="text-xl font-bold">Ola, Victor!</h2>
         <p>Terça-feira, 06 de agosto.</p>
 
+        {/* Busca */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça sua busca..." />
           <Button>
@@ -22,6 +26,7 @@ const Home = () => {
           </Button>
         </div>
 
+        {/* Imagem / Banner  */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             src="/banner-01.png"
@@ -30,6 +35,34 @@ const Home = () => {
             alt="Agende nos melhores com Barber Shop"
           />
         </div>
+
+        {/* Agendamento  */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
+          <CardContent className="flex justify-between p-0">
+            {/* Esquerda */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar>
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            {/* Direita */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">07</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
